8633edc6ce1551fd844380d415d5927fcf36864f
SMEH25 <smehendale@imsa.edu>
2025-06-08 20:32:37 -0500

    Changed default time for repeat

--- Discord_Bot/variables.py
+++ Discord_Bot/variables.py
@@ -1,5 +1,5 @@
 TOKEN = "" # Enter value
 CHANNEL_ID =  # Enter value
 TWITTER_USERNAMES = ["DeItaone"] # Separate list by commas
-RUN_INTERVAL_SECONDS = 5  # Time in seconds
+RUN_INTERVAL_SECONDS = 300  # Time in seconds
 TWEETS_PER_USER = 2 # Keep at a low number to reduce computation time
