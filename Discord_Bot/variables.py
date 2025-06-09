9d8345ee874455ab33c26144928607da19b524af
SMEH25 <smehendale@imsa.edu>
2025-06-08 20:30:01 -0500

    Fixed typo in twitter username

--- Discord_Bot/variables.py
+++ Discord_Bot/variables.py
@@ -1,5 +1,5 @@
-TOKEN = # Enter value
-CHANNEL_ID = # Enter value
-TWITTER_USERNAMES = ["deltaone"] # Separate list by commas 
+TOKEN = "" # Enter value
+CHANNEL_ID =  # Enter value
+TWITTER_USERNAMES = ["DeItaone"] # Separate list by commas
 RUN_INTERVAL_SECONDS = 5  # Time in seconds
 TWEETS_PER_USER = 2 # Keep at a low number to reduce computation time
